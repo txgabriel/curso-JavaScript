18f960f1c0886ff5a327a7ea269a3e2a61953323
txgabriel <gabriel.britopvh@hotmail.com>
2022-05-18 15:14:10 -0300

    aula 12

---
+++ aula 12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 16
+console.log(`você tem ${idade} anos.`)
+if (idade < 16) {
+    console.log('não vota')
+} else if (idade < 18 || idade > 64) {
+        console.log('voto opcional')
+} else  {
+    console.log('voto obrigátorio')
+} 
